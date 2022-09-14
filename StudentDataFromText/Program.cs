9439ab819feb99be7f4549900870d6701e864759
reddibhavyasri <reddibhavyasri@gmail.com>
2022-09-14 21:02:10 +0530

    changes upload

---
+++ StudentDataFromText/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StudentDataFromText
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = "E:\\Practice asnmnts\\StudentData.txt";
+            string text = File.ReadAllText(path);
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+
+    }
+}
